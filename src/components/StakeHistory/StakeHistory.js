1079c13d6f666ce89f8d676bf69c70e6e7ed10de
Fafemi Adeola <adeola5678@gmail.com>
2022-03-25 22:54:09 +0100

    updates

--- src/components/StakeHistory/StakeHistory.js
+++ src/components/StakeHistory/StakeHistory.js
@@ -12,7 +12,7 @@ const StakeHistory = ({
   stakeAddressAmount,
 }) => {
   return (
-    <div className={Styles.over}>
+    <div className={Styles.hide}>
       <form onSubmit={onClickBalance} className={Styles.form}>
         <input
           type="text"
@@ -29,42 +29,46 @@ const StakeHistory = ({
           <p className={Styles.text}>{stakeAddressAmount / 10 ** 18} BRT</p>
         )}
       </form>
-      <div className={Styles.root}>
-        <table className={Styles.table}>
-          <thead className={Styles.table_header}>
-            <tr className={Styles.table__head_row}>
-              <th className={Styles.table_head_data}>S/N</th>
-              <th className={Styles.table_head_data}>Amount Staked</th>
-              <th className={Styles.table_head_data}>Account</th>
-              <th className={Styles.table_head_data}>Action</th>
-              <th className={Styles.table_head_data}>Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {stakeData.map((item, index) => {
-              return (
-                <tr
-                  key={index}
-                  className={clsx({
-                    [Styles.table_row]: true,
-                    [Styles.unstake_style]: item.type === "unstake",
-                    [Styles.stake_style]: item.type === "stake",
-                  })}
-                >
-                  <td className={Styles.table_data}>{index + 1}</td>
-                  <td className={Styles.table_data}>
-                    {Number(utils.formatUnits(item.amount, 18)).toFixed(4)}
-                  </td>
-                  <td className={Styles.table_data}>
-                    {addressShortner(item.account, false)}
-                  </td>
-                  <td className={Styles.table_data}>{item.type}</td>
-                  <td className={Styles.table_data}>{formatDate(item.time)}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+      <div className={Styles.over}>
+        <div className={Styles.root}>
+          <table className={Styles.table}>
+            <thead className={Styles.table_header}>
+              <tr className={Styles.table__head_row}>
+                <th className={Styles.table_head_data}>S/N</th>
+                <th className={Styles.table_head_data}>Amount Staked</th>
+                <th className={Styles.table_head_data}>Account</th>
+                <th className={Styles.table_head_data}>Action</th>
+                <th className={Styles.table_head_data}>Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stakeData.map((item, index) => {
+                return (
+                  <tr
+                    key={index}
+                    className={clsx({
+                      [Styles.table_row]: true,
+                      [Styles.unstake_style]: item.type === "unstake",
+                      [Styles.stake_style]: item.type === "stake",
+                    })}
+                  >
+                    <td className={Styles.table_data}>{index + 1}</td>
+                    <td className={Styles.table_data}>
+                      {Number(utils.formatUnits(item.amount, 18)).toFixed(4)}
+                    </td>
+                    <td className={Styles.table_data}>
+                      {addressShortner(item.account, false)}
+                    </td>
+                    <td className={Styles.table_data}>{item.type}</td>
+                    <td className={Styles.table_data}>
+                      {formatDate(item.time)}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
